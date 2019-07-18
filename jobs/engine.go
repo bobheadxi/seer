555d27e6b067bf8433112b02eb79d62440fb9ce7
Robert Lin <robertlin1@gmail.com>
2019-07-17 22:58:34 -0700

    add engine state reports

--- jobs/engine.go
+++ jobs/engine.go
@@ -1,11 +1,13 @@
 package jobs
 
 import (
+	"fmt"
 	"runtime"
 	"time"
 
 	"github.com/gocraft/work"
 	"github.com/gomodule/redigo/redis"
+	"go.uber.org/multierr"
 	"go.uber.org/zap"
 
 	"go.bobheadxi.dev/seer/riot"
@@ -21,6 +23,7 @@ type Engine interface {
 type engine struct {
 	l    *zap.Logger
 	pool *work.WorkerPool
+	jobs *work.Client
 }
 
 // BaseJobContext denotes dependencies common to most jobs
@@ -52,12 +55,21 @@ func NewJobsEngine(l *zap.Logger, app string, redisPool *redis.Pool, b *BaseJobC
 	return &engine{
 		l:    l,
 		pool: pool,
+		jobs: work.NewClient(app, redisPool),
 	}
 }
 
 func (e *engine) Start() {
 	e.l.Info("starting job engine")
 	e.pool.Start()
+
+	// spin up status reporter
+	var check int
+	interval := time.NewTicker(5 * time.Minute) // TODO: configure?
+	for range interval.C {
+		e.reportState(check)
+		check++
+	}
 }
 
 func (e *engine) Close() {
@@ -65,4 +77,39 @@ func (e *engine) Close() {
 	t := time.AfterFunc(30*time.Second, e.pool.Stop)
 	e.pool.Drain()
 	t.Stop()
+}
+
+func (e *engine) reportState(id int) {
+	var errors error
+	heartbeats, err := e.jobs.WorkerPoolHeartbeats()
+	if err != nil {
+		multierr.Append(errors, fmt.Errorf("could not get hearbeats: %v", err))
+	}
+
+	// TODO: report more details if desired?
+	_, pagesDead, err := e.jobs.DeadJobs(0)
+	if err != nil {
+		multierr.Append(errors, fmt.Errorf("could not get dead jobs: %v", err))
+	}
+	_, pagesScheduled, err := e.jobs.ScheduledJobs(0)
+	if err != nil {
+		multierr.Append(errors, fmt.Errorf("could not get scheduled jobs: %v", err))
+	}
+	_, pagesRetry, err := e.jobs.RetryJobs(0)
+	if err != nil {
+		multierr.Append(errors, fmt.Errorf("could not get retry jobs: %v", err))
+	}
+
+	// report state
+	e.l.Info("engine state report",
+		zap.Int("jobstatereport.id", id),
+		zap.Any("heartbeats", heartbeats),
+		zap.Int64("dead_jobs", pagesDead*20),
+		zap.Int64("scheduled_jobs", pagesScheduled*20),
+		zap.Int64("retry_jobs", pagesRetry))
+	if errors != nil {
+		e.l.Error("error(s) encountered on engine state report",
+			zap.Int("jobstatereport.id", id),
+			zap.Error(errors))
+	}
 }
