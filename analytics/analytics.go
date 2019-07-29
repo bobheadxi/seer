209f148b03d537f353862f47c7e03f833dd089a2
Robert Lin <robertlin1@gmail.com>
2019-07-28 17:38:04 -0700

    scaffold potential analytics output

--- analytics/analytics.go
+++ analytics/analytics.go
@@ -1,30 +1,71 @@
 package analytics
 
 type Analytics struct {
-	Team    *TeamAnalytics              `json:"team"`
-	Players map[string]*PlayerAnalytics `json:"players"`
+	Team    map[string]*TeamAnalytics              `json:"team"`    // per season
+	Players map[string]map[string]*PlayerAnalytics `json:"players"` // per season, per player
 }
 
 type PlayerAnalytics struct {
-	Overview     *PlayerOverview     `json:"overview"`
+	Tier         string              `json:"season_tier"`
 	Aggregations *PlayerAggregations `json:"aggregations"`
 }
 
-type PlayerOverview struct {
-	SeasonTier string `json:"season_tier"`
-	PeakTier   string `json:"peak_tier"`
+type PlayerAggregations struct {
+	Lanes []string `json:"lanes"` // top 2
+	Roles []string `json:"roles"` // top 2
+
+	Champions map[string]ChampionAggregation `json:"champions"` // per champion in top 5
 }
 
-type PlayerAggregations struct {
+type Aggregate struct {
+	Average float64 `json:"average"`
+	Max     float64 `json:"max"`
+	Min     float64 `json:"min"`
+	StdDev  float64 `json:"stddev"`
+}
+
+type Perks struct {
+	Primary   string
+	Secondary string
+}
+
+type Summoners struct {
+	Spell1 string
+	Spell2 string
+}
+
+type ChampionAggregation struct {
+	Lane string
+	Role string
+
+	Perks     Perks
+	Summoners Summoners
+	Items     []string
+
+	Games     GameAggregations
+	Timelines TimelineAggregations
+}
+
+type TimelineAggregations struct {
+	// TODO
+}
+
+type GameAggregations struct {
+	Kills   Aggregate
+	Deaths  Aggregate
+	Assists Aggregate
+
+	Vision Aggregate
+	Gold   Aggregate
+
+	DamageDealt Aggregate
+	DamageTaken Aggregate
+
+	Minions        Aggregate
+	JungleFriendly Aggregate
+	JungleEnemy    Aggregate
 }
 
 type TeamAnalytics struct {
-	Overview     *TeamOverview     `json:"overview"`
-	Aggregations *TeamAggregations `josn:"aggregations"`
-}
-
-type TeamOverview struct {
-}
-
-type TeamAggregations struct {
+	// TODO
 }
