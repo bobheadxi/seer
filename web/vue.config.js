1075e00ace95ff181b7dbd891742e608e8bb1193
Robert Lin <robertlin1@gmail.com>
2019-08-21 12:44:46 -0700

    init antd vue

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,8 +1,17 @@
 const path = require('path');
+const darkTheme = require('@ant-design/dark-theme');
 
 module.exports = {
   outputDir: path.resolve(__dirname, '../docs'),
   configureWebpack: (config) => {
     config.devtool = 'source-map'; // eslint-disable-line
+  },
+  css: {
+    loaderOptions: {
+      less: {
+        modifyVars: darkTheme,
+        javascriptEnabled: true,
+      },
+    },
   },
 };
